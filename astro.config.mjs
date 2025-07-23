9143246451a37592edad3699e4a27bf444b38599
0nn7a <mohonaney@gmail.com>
2025-07-23 16:22:15 +0800

    Finish!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   base: 'STUDY-astro-tutorial',
+  site: 'https://0nn7a.github.io/STUDY-astro-tutorial',
   outDir: './dist',
   output: 'static',
+  integrations: [preact()],
 });
